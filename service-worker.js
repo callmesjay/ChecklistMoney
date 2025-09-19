f63513ca88d4c1df4afafb8d29e56091fc88c6a2
callmesjay <bbbew2005@gmail.com>
2025-09-19 18:40:28 +0700

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'expense-tracker-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
